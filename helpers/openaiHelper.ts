06e60cd79bfd4fd1caf9bf53eade15d7a89b414b
Kevin Kujawski <kkujawski@buk.cl>
2025-03-22 22:22:21 -0300

    feat: swap

--- helpers/openaiHelper.ts
+++ helpers/openaiHelper.ts
@@ -8,7 +8,7 @@ export const processTextsWithAI = async (texts: string[]): Promise<TransactionRe
 
   const prompt = `
   Interpret the following text and extract the following fields in JSON format:
-  - action: should be one of ["transfer", "invest"]. If no valid action is found, return an error.
+  - action: should be one of ["transfer", "invest", "swap"]. If no valid action is found, return an error.
   - amount: the numeric value being transferred, swapped or invested.
   - token: the currency/token being used.
   - chain: the chain of the transaction [ethereum, base, zksync, mantle, stellar, polkadot, worldchain].
@@ -19,6 +19,10 @@ export const processTextsWithAI = async (texts: string[]): Promise<TransactionRe
   If invest, extract the following fields:
   - to: the recipient of the investment, one of ['aave', 'lendle'].
   
+  If swap, extract the following fields:
+  - tokenFrom: the token being sold.
+  - tokenTo: the token being bought.
+
   Only return the JSON, nothing else. Without any other text or comments.
   In the "description" field, return a description of the transaction, describing deeply the future transaction.
   In the "message" field, return a message to the user, describing the transaction.
